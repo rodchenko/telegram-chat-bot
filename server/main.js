54fbbb57c42dfa6c78a6dea4e7b343b5f7a693ba
Valia Fetisov <valyalof@gmail.com>
2016-04-04 19:55:56 +0300

    search images on flickr

--- server/main.js
+++ server/main.js
@@ -1,28 +1,33 @@
 import { Meteor } from 'meteor/meteor';
 import TelegramBot from 'node-telegram-bot-api';
+import Flickr from 'flickrapi';
 
-Meteor.startup(() => {
-  // true
-  // "ffasdfaa adf adf"
-  // 'e'
-  // var value = 121323223 + "string" // "121323223string"
-  // {
-  //   key: function() {
-  //     return;
-  //   }
-  // }
-  console.log( "photo please".indexOf('photo') );
+var flickr;
+Flickr.tokenOnly({
+  api_key: "720dc6be11c683f7351772045c4ed4c9",
+  secret: "1f8ec837ddc37e06"
+}, function(error, result) {
+  flickr = result;
 });
 
 var token = '191390975:AAG2c4_JHIRtH7Jq4s2ogEzgzS7pAC-QeSw';
 var bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', function (msg) {
+
   // console.log(msg);
   var chatId = msg.chat.id;
-  if(msg.text.toLowerCase().indexOf('photo') >= 0) {
-    bot.sendMessage(chatId, "no photo");
-  } else {
-    // bot.sendMessage(chatId, "");
-  }
+
+  flickr.photos.search({
+    text: msg.text
+  }, function(err, result) {
+    if(err) { throw new Error(err); }
+    var photo = result.photos.photo[1]
+    console.log('result', 'http://www.flickr.com/photos/' + photo.id + "_" + photo.secret + ".jpg");
+    console.log('result', 'http://www.flickr.com/photos/eric/'+photo.id+'/');
+    // http://www.flickr.com/photos/132375_0ca82ae31e.jpg
+    var link = 'http://www.flickr.com/photos/' + photo.id + "_" + photo.secret + ".jpg";
+    bot.sendMessage(chatId, '<a href="'+link+'">'+photo.title+'</a>', {parse_mode: "HTML"});
+  });
+
 });
